3312d9a9d8f6d3b9883fe0083d873d981e34ff63
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-04-21 10:45:58 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -19,7 +19,7 @@ if (cold) {
   console.log("Short sleeves are fine.");
 }*/
 
-const temperature = 18;
+/*const temperature = 18;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -29,4 +29,20 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
+/*
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}*/
+
+/*let temperature = 45;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+} */
+const raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
