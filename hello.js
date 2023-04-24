d724b5aaf6b33d2008f424aa088e060b4511f6da
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-04-23 23:26:24 -0700

    updates

--- hello.js
+++ hello.js
@@ -4,7 +4,18 @@
 //
 //sayHello();
 
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
-sayHello("Marina")
+sayHello("Marina")*/
+/*
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');*/ 
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
