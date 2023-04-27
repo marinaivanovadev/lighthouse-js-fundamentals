45682d11d5fe0865e43b1da671c373ba847f33e3
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-04-26 23:40:24 -0700

    Age

---
+++ ageCalculator.js
@@ -0,0 +1,13 @@
+let name = " ";
+let yearOfBirth = 1900;
+let currentYear = 2023;
+
+function ageCalculator(name, yearOfBirth, currentYear)
+  {
+
+return name +" is "+ (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015))
+ageCalculator("Suzie", 1983, 2015);
