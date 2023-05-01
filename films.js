809971c916a974f23e7e4aef8a816943ad7746e1
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-04-30 20:50:25 -0700

    films

---
+++ films.js
@@ -0,0 +1,12 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+console.log("My favourite " + director + " film is " + films[1]);
+console.log("films at index 0: " + films[0]);
+"films at index 1: " + films[1];
+"films at index 2: " + films[2];
+"films at index 3: " + films[3];
+
+films.push("A.I.");
+films.push("E.T.");
+director + " is the director of " + films.length + " films";
+console.log(director + " is the director of " + films.length + " films");
