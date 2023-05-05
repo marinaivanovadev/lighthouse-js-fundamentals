40653b4b11f2822cd3796ef268ee52cfad712130
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-05-04 21:54:03 -0700

    kata7

---
+++ kata7.js
@@ -0,0 +1,29 @@
+const checkAir = function (samples, threshold) {
+  
+  let dirtySamples = 0;
+  for (let i = 0; i < samples.length; i++){
+    if (samples[i] === 'dirty') {
+      dirtySamples ++;
+    }
+  }
+  let result = dirtySamples / samples.length;
+    if (result > threshold) {
+      return "Polluted"
+    } else {
+      return "Clean";
+    }
+  };
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
